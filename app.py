c3cfbb94f0cfb3629c0b6d42913e66691630deba
Mosh Sar <moshiksar@gmail.com>
2020-03-19 13:36:43 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Init app
+
+app = Flask(__name__)
+baserdir = os.path.abspath(os.path.dirname(__file__))
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(baserdir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init db
+db = SQLAlchemy(app)
+
+#Init ma
+ma = Marshmallow(app)
+
+# Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
+#Product schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'description', 'price', 'qty')
+
+# Init scema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many = True)
+
+# Create a Product
+@app.route('/product', methods=['POST'])
+def add_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name, description, price, qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+# Update a Product
+@app.route('/product/<id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    product.name = request.json['name']
+    product.description = request.json['description']
+    product.price = request.json['price']
+    product.qty = request.json['qty']
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+#Get All Products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+#Get Single Product
+@app.route('/product/<id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)
+
+#Delete Single Product
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    
+    return product_schema.jsonify(product)
+
+#Delete All Products
+@app.route('/product', methods=['DELETE'])
+def delete_all_product():
+    all_products = Product.query.all()
+    for p in all_products:
+        db.session.delete(p)
+    db.session.commit()
+    
+    return product_schema.jsonify(all_products)
+
+# Home
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg' : 'Hello World'})
+
+#Run Server
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
